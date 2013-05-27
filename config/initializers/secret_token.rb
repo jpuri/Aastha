6a274258ef159211d7e72778cc4ff5f454863588
jpuri <jyotipuri@gmail.com>
2013-05-27 12:01:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aastha::Application.config.secret_token = '3402a58e8ebebce545efd8edca683b26f6dc09b488682c703ef9a1b74408cb493d1f98bf3c561d9a717ce2d734e56258a75d9cab1213948dd4b8f07f19e0dbf1'
